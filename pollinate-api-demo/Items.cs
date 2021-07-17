3500b912aeff7648a2f6eca923640ffd3c528f8b
Paul Cardus <cardusuk@outlook.com>
2021-07-17 19:52:09 +0100

    Initial commit

---
+++ pollinate-api-demo/Items.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace pollinate_api_demo
+{
+    public class Item
+    {
+        [JsonPropertyName("id")]
+        public string id { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("dateTime")]
+        public DateTime dateTime { get; set; }
+    }
+}
